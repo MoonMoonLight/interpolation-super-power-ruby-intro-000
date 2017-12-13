bc9af788787de8a3f3678ce311e609e0ccf4c386
Priscila Moreno <priscila.moreno.wow@gmail.com>
2017-12-13 01:14:07 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 def display_rainbow(colors)
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, Violet: #{colors[6]}"
-  puts "Contact #{person[3]}"
+  
 
 end# Write your #display_rainbow method here
 
-["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+#["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
